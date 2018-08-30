61342c009a84742ca1bacd472df3b97db004f436
Thomas <thomas99k@gmail.com>
2018-08-30 09:16:34 -0400

    core - update sentinel script to only mail on error

--- sentinel.sh
+++ sentinel.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 path="$( cd "$(dirname "$0")" ; pwd -P )"
-power=$(pmset -g batt | grep -o '[0-9][0-9]%')
+power=$(pmset -g batt | grep -o '[0-9]*%')
 power=${power//[!0-9]/}
 
 if [ "$power" -lt 20 ]
 then
-	echo "Battery at $power%, alerting user..."
 	sh $path/alert.sh
-else
-	echo "Battery at $power%, no alert needed."
 fi
